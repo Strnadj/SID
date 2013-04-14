80f30fdae068e6fd94afe56dad04346905a66c02
Strnadj <jan.strnadek@gmail.com>
2013-04-14 12:51:21 +0200

    Majority part of text of bc work, update tests, update ruby code

--- RubyProject/lib/tester/url_tester.rb
+++ RubyProject/lib/tester/url_tester.rb
@@ -26,43 +26,46 @@ module ContentTester
       return ret
     end
 
-    # Method for start test
-    def start_test()
-      print "= Start testing: =\n".bold.green if @options[:debug]
+    # Link testing
+    # @return nil
+    def test_links()
+     # Parse links
+        puts "Test links:".bold.red << "\n" if options[:debug]
+        @parser_results[:links].each {
+            |link|
+            attr = link.attr
 
-      # Parse links
-      puts "Test links:".bold.red << "\n" if options[:debug]
-      @parser_results[:links].each {
-        |link|
-        attr = link.attr
+            puts "\tTest: ".yellow.bold << link.url << "\n" if @options[:debug]
 
-        puts "\tTest: ".yellow.bold << link.url << "\n" if @options[:debug]
+            for i in 0..(attr.count - 1)
+              # Copy array
+              attr_c = Marshal.load(Marshal.dump(attr))
 
-        for i in 0..(attr.count - 1)
-          # Copy array
-          attr_c = Marshal.load(Marshal.dump(attr))
+              # Add to paramaetr test case
+              attr_c[i][1] += "'"
 
-          # Add to paramaetr test case
-          attr_c[i][1] += "'"
+              # Try to get page
+              original = get_request(link.get_uri())
+              test_res = get_request(create_url(link.url, attr_c))
 
-          # Try to get page
-          original = get_request(link.get_uri())
-          test_res = get_request(create_url(link.url, attr_c))
+              # Test 404 error
+              if original[:code] == 404 || test_res[:code] == 404
+                puts "\t\tError: ".red.bold << "404 - not found" << "\n" if @options[:debug]
+                next
+              end
 
-          # Test 404 error
-          if original[:code] == 404 || test_res[:code] == 404
-            puts "\t\tError: ".red.bold << "404 - not found" << "\n" if @options[:debug]
-            next
-          end
-
-          # Test parameters
-          if self.compare_results(original[:body], test_res[:body])
-            puts "\t\tParam: ".red.bold << attr_c[i][0] << " - probably " << "UNSECURED".bold.red << "\n"  if @options[:debug]
-            puts "\t\t" << create_url(link.url, attr_c).to_s << "\n\n" if @options[:debug]
-          end
-        end
+              # Test parameters
+              if self.compare_results(original[:body], test_res[:body])
+                puts "\t\tParam: ".red.bold << attr_c[i][0] << " - probably " << "UNSECURED".bold.red << "\n"  if @options[:debug]
+                puts "\t\t" << create_url(link.url, attr_c).to_s << "\n\n" if @options[:debug]
+              end
+            end
       }
+    end
 
+    # Method for testing get forms
+    # @return nil
+    def test_get_forms()
       # Parse get forms
       puts "Test get forms:".bold.green << "\n" if @options[:debug]
       @parser_results[:get_forms].each {
@@ -121,7 +124,23 @@ module ContentTester
           end
         end
       }
+    end
 
+    # Method for testing post forms
+    # @return nil
+    def test_post_forms()
+        
+    end
+
+    # Method for start test
+    # @return nil
+    def start_test()
+      print "= Start testing: =\n".bold.green if @options[:debug]
+
+      # Run start testing a
+      test_links()
+
+   
       # Parse post forms
       puts "Test post forms:".bold.green << "\n" if @options[:debug]
       @parser_results[:post_forms].each {
@@ -235,7 +254,7 @@ module ContentTester
     def compare_results(before, after)
       if options[:error_directive]
         # True - try to find ERROR message on site
-        return after.include? "Warning: mysql_"
+        return (after.include? "Warning: mysql_") ||  (after.include? "Error: mysql_") ||  (after.include? "Fatal Error: mysql_") || (after.include? "error: mysql_")
       else
         # False - try to compare results on page
       end
